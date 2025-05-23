e48342c73d184d575a2f1d37fd836d565daaefce
ANireshkumar <niresh1606@gmail.com>
2025-05-23 11:15:39 +0530

    register page and login page design done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,19 @@ import NavBar from "../components/NavBar";
 const HomePage = () => {
     return (
         <>
-            <NavBar />
-            <h1>HomePage</h1>
+            <div className="container mt-5 text-center">
+                <h1 className="text-4xl text-gray-800">Welcome to recipe APP!</h1>
+                <h4 className="text-xl text-gray-600">A simple recipe application</h4>
+
+                <div className="max-w-2xl mx-auto mt-5 bg-white shadow-md p-5 rounded-lg overflow-hidden border border-gray-200 px-5 py-5">
+                    <ul className="text-center mt-1 text-lg text-gray-600">
+                        <li>Share your recipe with your friends</li>
+                        <li>Recipe Discovery and cook as you like</li>
+                        <li>Save your favorite recipes</li>
+                        <li>Likes and comments</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
