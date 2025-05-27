31d14f00ffef161974bcecb336f9c619b6e62788
ANireshkumar <niresh1606@gmail.com>
2025-05-27 11:29:43 +0530

    post like and unlike done --syncing to the icons pending

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,15 +4,15 @@ const HomePage = () => {
     return (
         <>
             <div className="container mt-5 text-center">
-                <h1 className="text-4xl text-gray-800">Welcome to recipe APP!</h1>
-                <h4 className="text-xl text-gray-600">A simple recipe application</h4>
+                <h1 className="text-4xl text-gray-800">Welcome to Chattr!</h1>
+                <h4 className="text-xl text-gray-600">A simple chat application</h4>
 
                 <div className="max-w-2xl mx-auto mt-5 bg-white shadow-md p-5 rounded-lg overflow-hidden border border-gray-200 px-5 py-5">
                     <ul className="text-center mt-1 text-lg text-gray-600">
-                        <li>Share your recipe with your friends</li>
-                        <li>Recipe Discovery and cook as you like</li>
-                        <li>Save your favorite recipes</li>
-                        <li>Likes and comments</li>
+                        <li>Chat with your friends</li>
+                        <li>Send messages to your friends</li>
+                        <li>View messages from your friends</li>
+                        <li>And much more!</li>
                     </ul>
                 </div>
             </div>
